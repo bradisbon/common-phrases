4cbe994767ace45e3e32e4a28eccbfe8d465cf5b
brad <bradisbon@gmail.com>
2022-06-02 00:10:55 -0500

    main.py:   - initialize api   - define request and response objects   - define find_common post handler

---
+++ common_phrases/main.py
@@ -0,0 +1,35 @@
+import re
+from collections import Counter
+from typing import List
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+api = FastAPI()
+
+
+class Text(BaseModel):
+    text: str
+
+
+class MostCommon(BaseModel):
+    most_common: List[str]
+
+
+def clean(text: str) -> List[str]:
+    """Replace newlines with spaces, remove punctuation, and convert to lowercase"""
+
+    no_newlines = text.replace('\n', ' ')
+    no_punc = re.sub(r"[^a-z ]+", '', no_newlines, flags=re.I)
+    lower = no_punc.lower()
+
+    return lower.split(' ')
+
+
+@api.post('/find_common', response_model=MostCommon)
+async def find_common(text: Text):
+    clean_text = clean(text.text)
+    counts = Counter((' '.join(clean_text[i:i+3]) for i,_ in enumerate(clean_text[:-2])))
+
+    return {'most_common': (p[0] for p in counts.most_common(100))}
